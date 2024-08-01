18e9f7c5cbf3d8cee02b189f50b30bb9e254d979
Anais <felfelanais@gmail.com>
2024-08-01 23:20:41 +0200

    MOdifier le projet

---
+++ src/Controller/IngredientController.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\Ingrediant;
+use App\Form\IngrediantType;
+use App\Service\ProductService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IngredientController extends AbstractController
+{
+    private $productService;
+
+    public function __construct(ProductService $productService)
+    {
+        $this->productService = $productService;
+    }
+
+    #[Route('/ingredient/new', name: 'ingredient_new')]
+    public function new(Request $request): Response
+    {
+        $ingredient = new Ingrediant();
+        $form = $this->createForm(IngrediantType::class, $ingredient);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->productService->createIngredient(
+                $ingredient->getName(),
+                $ingredient->getQuantity()
+            );
+
+            return $this->redirectToRoute('ingredient_success');
+        }
+
+        return $this->render('create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/ingredient/success', name: 'ingredient_success')]
+    public function success(): Response
+    {
+        return $this->render('update.html.twig');
+    }
+}
